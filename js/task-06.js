5db06bc3d9daa67abfa9d17b40e7a017d5d55956
Maxim Kazachuk <dem2bb1@gmail.com>
2020-12-13 10:59:23 +0200

    Добавлена 6 дз

--- js/task-06.js
+++ js/task-06.js
@@ -1,4 +1,22 @@
 let input;
 let total = 0;
 
-let userInput = prompt('Введите число');
+while (true) {
+  input = prompt('Введите число');
+
+  if (input === null) {
+    break;
+  }
+
+  input = Number(input);
+  const notANumber = Number.isNaN(input);
+
+  if (notANumber) {
+    alert('Было введено не число, попробуйте еще раз!');
+    continue;
+  }
+
+  total += input;
+}
+
+alert(`Общая сумма чисел равна ${total}`);
